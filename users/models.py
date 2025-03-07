fd0a32df77f3dfb36633568a05c355a99a6cbdb4
qazcde <qazcde.k@gmail.com>
2025-03-08 08:21:28 +0900

    modify users admin

--- users/models.py
+++ users/models.py
@@ -3,4 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False, blank=True)
+    last_name = models.CharField(max_length=150, editable=False, blank=True)
+    name = models.CharField(max_length=150, default="")
+    is_host = models.BooleanField(default=False)
